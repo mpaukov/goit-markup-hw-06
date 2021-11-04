0dda832c2524ec67d0a9521f6062c829f43c5bd4
Michael Paukov <michael.paukov@gmail.com>
2021-11-04 17:17:05 +0100

    Add some changes

--- js/main.js
+++ js/main.js
@@ -1,6 +1,13 @@
 (() => {
-  const   { height: pageHeaderHeight } = document
-        .querySelector(".header")
-        .getBoundingClientRect();
-    document.body.style.paddingTop = `${pageHeaderHeight}px`;
+  const refs = {
+    formsend: document.querySelector('.js-speaker-form')};
+
+  refs.formsend.addEventListener('submit', e => {
+    e.preventDefault();
+    new FormData(e.currentTarget).forEach((value, name) =>
+      console.log(`${name}: ${value}`));
+    e.currentTarget.reset();
+  });
 })();
+
+
--- js/modal.js
+++ js/modal.js
@@ -4,10 +4,12 @@
     closeModalBtn: document.querySelector('[data-modal-close]'),
     modal: document.querySelector('[data-modal]'),
     formmodal: document.querySelector('[data-form-modal]')
+   
   };
 
   refs.openModalBtn.addEventListener('click', toggleModal);
   refs.closeModalBtn.addEventListener('click', toggleModal);
+ 
 
   function toggleModal() {
     refs.modal.classList.toggle('is-hidden');
