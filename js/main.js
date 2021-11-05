7ab5864a6b65d1a2dbc049f85c1288b5fa63985e
Michael Paukov <michael.paukov@gmail.com>
2021-11-05 11:43:34 +0100

    some changes

--- js/main.js
+++ js/main.js
@@ -1,8 +1,16 @@
 (() => {
   const refs = {
-    formsend: document.querySelector('.js-speaker-form')};
+    formsend: document.querySelector('.js-speaker-form'),
+    modalsend:document.querySelector('.modal-container')};
 
   refs.formsend.addEventListener('submit', e => {
+    e.preventDefault();
+    new FormData(e.currentTarget).forEach((value, name) =>
+      console.log(`${name}: ${value}`));
+    e.currentTarget.reset();
+  });
+
+   refs.modalsend.addEventListener('submit', e => {
     e.preventDefault();
     new FormData(e.currentTarget).forEach((value, name) =>
       console.log(`${name}: ${value}`));
